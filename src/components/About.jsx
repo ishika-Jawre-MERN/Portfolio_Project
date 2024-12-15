9365c7dcbb0956fb6a40bb617bf80df307bb9241
ishiijawre <ishikajawre@gmail.com>
2024-12-15 19:19:38 +0530

    Initial commit

---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5 text-indigo-700">About</h1>
+        <p>
+          I am an individual with a strong passion for web development. My skill
+          set includes , HTML, CSS, React.js, and JavaScript, bootstrap,
+          tailwind which enables me to excel in these fields. My ability to
+          create dynamic web experiences makes me a valuable asset to any
+          project. I have a variety of interests outside of my professional
+          skills. I'm an avid reader and enjoy exploring new worlds through
+          literature. When I'm not reading or coding, I like to travel to new
+          places, dance, or cook up delicious dishes in the kitchen. My diverse
+          interests and dedication to my craft make me a well-rounded and
+          passionate individual, ready to take on the world of web development.
+        </p>
+        <br />
+        <h1 className="text-indigo-700 font-semibold text-xl">
+          Education Details
+        </h1>
+        <span>
+          2020 - 2024 <br />
+          B.TECH BMCT Indore, IN
+        </span>
+        <br />
+        <br />
+        <h1 className="text-indigo-700 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in HTML ,CSS ,JS ,React Js ,Node.js ,Tailwind CSS , Mongo
+          Db <br />
+          Good in problem-solving skills and good collaborator
+        </span>
+        <br />
+        <br />
+        <h1 className="text-indigo-700 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Business Development Executive||Edithtech Softwares . July2023 - Nov
+          2023· Dewas, Madhya Pradesh, India · On-site
+          <br />
+          <br />
+          Web Developer ||The One Web Technology · Aug 2024 - Present · 5 mos
+          Vadodara, Gujarat, India · Remote
+        </span>
+        <br />
+        <br />
+        <h1 className="text-indigo-700 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative IT solutions that exceed client expectations and contribute
+          positively to the digital landscape. I am committed to continuous
+          learning and growth, always seeking new challenges and opportunities
+          to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import pic from "../assets/img.jpg";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoCloseSharp } from "react-icons/io5";
+import { Link } from "react-scroll";
+function Navbar() {
+  const [menu, setMenu] = useState(false);
+  const navItems = [
+    {
+      id: 1,
+      text: "Home",
+    },
+    {
+      id: 2,
+      text: "About",
+    },
+    {
+      id: 3,
+      text: "Portfolio",
+    },
+    {
+      id: 4,
+      text: "Experiance",
+    },
+    {
+      id: 5,
+      text: "Contact",
+    },
+  ];
+  return (
+    <>
+      <div className="w-full py-4 px-4 md:px-20 h-24 shadow-md fixed top-0 left-0 right-0 z-50  bg-gradient-to-r from-indigo-500 via-purple-500  via bg-indigo-500 text-white ">
+        <div className="flex justify-between items-center h-16">
+          <div className=" flex space-x-2">
+            <img src={pic} className="h-12 w-12 rounded-full" alt="" />
+            <h1 className="font-semibold text-2xl cursor-pointer">
+              Ishika<span className="text-indigo-950 text-2xl">Jawre</span>
+              <p className="text-sm">Web Developer</p>
+            </h1>
+          </div>
+          {/* desktop navbar */}
+          <div>
+            <ul className="hidden md:flex space-x-8">
+              {navItems.map(({ id, text }) => (
+                <li
+                  className="hover:scale-105 duration-200 cursor-pointer"
+                  key={id}
+                >
+                  <Link
+                    to={text}
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    activeClass="active"
+                  >
+                    {text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <div onClick={() => setMenu(!menu)} className="md:hidden">
+              {menu ? <IoCloseSharp size={24} /> : <AiOutlineMenu size={24} />}
+            </div>
+          </div>
+        </div>
+        {/* mobile navbar */}
+        {menu && (
+          <div className="bg-gradient-to-r from-indigo-500 via-purple-500  via bg-indigo-500">
+            <ul className="md:hidden flex flex-col h-screen items-center justify-center space-y-3 text-4xl ">
+              {navItems.map(({ id, text }) => (
+                <li
+                  className="hover:scale-105 duration-200 font-semibold cursor-pointer"
+                  key={id}
+                >
+                  <Link
+                    onClick={() => setMenu(!menu)}
+                    to={text}
+                    smooth={true}
+                    duration={500}
+                    offset={-70}
+                    activeClass="active"
+                  >
+                    {text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
